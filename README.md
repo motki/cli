0a8f866acb6f1391f8a21e76d7d8d8969a43d133
Tyler Sommer <sommertm@gmail.com>
2017-09-21 19:32:17 -0600

    Extract motki command. Initial commit

---
+++ README.md
@@ -0,0 +1,54 @@
+# motki-cli
+
+Command `motki` contains interactive command-line tools for managing EVE Online character and corporation assets and industrial processes.
+
+## Getting started
+
+Install `motki` using `go get`.
+
+```bash
+go get -u github.com/motki/motki-cli/...
+```
+
+Run the program.
+
+```bash
+motki
+```
+
+> Note that the default configuration connects to `motki.org:18443` using SSL.
+
+### Command-line options
+
+```
+Usage of motki:
+  -credentials string
+    	Username and password separated by a colon. (ie. "frank:mypass")
+  -history-file string
+    	Path to the CLI history file. (default ".history")
+  -insecure-skip-verify
+    	INSECURE: Skip verification of server SSL cert.
+  -log-level string
+    	Log level. Possible values: debug, info, warn, error. (default "warn")
+  -server string
+    	Backend server host and port. (default "motki.org:18443")
+  -version
+    	Display the application version.
+```
+
+## Authenticating
+
+Some functionality in the application requires authenticating with the remote motkid installation (by default, the [Moritake Industries](https://moritakeindustries.com) website).
+
+To authenticate:
+
+1. Ensure you have a valid account with characters linked on the remote motkid installation.
+2. Configure `motki` to use your credentials.
+    a. Pass them via command-line option:
+       ```
+       motki -credentials username:password
+       ```
+    b. Pass them via environment variables:
+       ```
+       MOTKI_USERNAME=username MOTKI_PASSWORD=password motki
+       ```
---
+++ cmd/motki/main.go
@@ -0,0 +1,81 @@
+// Command motki is a utility for interacting with the various data structures
+// and processes in the overall motkid project.
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+	"strings"
+
+	"github.com/motki/motkid/app"
+	"github.com/motki/motkid/log"
+	"github.com/motki/motkid/model"
+	"github.com/motki/motkid/proto/client"
+)
+
+var serverAddr = flag.String("server", "motki.org:18443", "Backend server host and port.")
+var historyPath = flag.String("history-file", ".history", "Path to the CLI history file.")
+var credentials = flag.String("credentials", "", "Username and password separated by a colon. (ie. \"frank:mypass\")")
+var logLevel = flag.String("log-level", "warn", "Log level. Possible values: debug, info, warn, error.")
+var insecureSkipVerify = flag.Bool("insecure-skip-verify", false, "INSECURE: Skip verification of server SSL cert.")
+var version = flag.Bool("version", false, "Display the application version.")
+
+// fatalf creates a default logger, writes the given message, and exits.
+func fatalf(format string, vals ...interface{}) {
+	logger := log.New(log.Config{})
+	logger.Fatalf(format, vals...)
+}
+
+var Version = "dev"
+
+func main() {
+	flag.Parse()
+	if *version {
+		fmt.Printf("%s %s. %s\n", os.Args[0], Version, "https://github.com/motki/motki-cli")
+		os.Exit(0)
+	}
+
+	appConf := &app.Config{
+		Backend: model.Config{
+			Kind: model.BackendRemoteGRPC,
+			RemoteGRPC: model.RemoteConfig{
+				ServerAddr:         *serverAddr,
+				InsecureSkipVerify: *insecureSkipVerify,
+			},
+		},
+		Logging: log.Config{
+			Level: *logLevel,
+		},
+	}
+
+	// Writing to stderr offers a way to redirect the logger output to a file instead of
+	// interrupting the user.
+	appConf.Logging.OutputType = log.OutputStderr
+
+	conf := app.NewCLIConfig(appConf)
+	if *credentials != "" {
+		parts := strings.Split(*credentials, ":")
+		if len(parts) != 2 {
+			fatalf("motki: invalid credentials, expected format \"username:password\"")
+		}
+		conf = conf.WithCredentials(parts[0], parts[1])
+	} else {
+		user, pass := os.Getenv("MOTKI_USERNAME"), os.Getenv("MOTKI_PASSWORD")
+		if user != "" && pass != "" {
+			conf = conf.WithCredentials(user, pass)
+		}
+	}
+
+	env, err := app.NewCLIEnv(conf, *historyPath)
+	if err != nil {
+		if err == client.ErrBadCredentials {
+			fmt.Println("Invalid username or password.")
+		}
+		fatalf("motki: error initializing application environment: %s", err.Error())
+	}
+
+	go env.LoopCLI()
+
+	env.BlockUntilAbort(nil)
+}
