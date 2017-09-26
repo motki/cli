13eff9a202c224647662d0fb30e1c9b06314e8d1
Tyler Sommer <sommertm@gmail.com>
2017-09-26 02:37:44 -0600

    Add root doc.go

---
+++ doc.go
@@ -0,0 +1,6 @@
+// Package motki_cli contains a command-line interface for interacting with
+// a remote motki application server.
+//
+// This project contains the motki command source code, as well text-processing
+// functionality.
+package motki_cli
